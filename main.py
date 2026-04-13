7494709c34315d67fa88310c2e6959b521f0a55d
Shubham Agrawal <shubhamisme7@gmail.com>
2026-04-13 07:34:24 +0530

    force CORS allow all origins

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="BookBugs Review Service")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.environ.get("ALLOWED_ORIGINS", "*").split(","),
+    allow_origins=["*"],
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
